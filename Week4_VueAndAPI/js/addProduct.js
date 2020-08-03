30cd3653f8ffa84144a5cf522812d2495e9e453f
Raindot <sakuhime24@gmail.com>
2020-08-03 17:43:37 +0800

    create product

--- Week4_VueAndAPI/js/addProduct.js
+++ Week4_VueAndAPI/js/addProduct.js
@@ -2,12 +2,20 @@ export default {
   data : function () {
     return {
       newProduct: {
-        title: '',
-        category: '',
-        unit: '',
-        origin_price: '',
-        price: ''
+        title: '1',
+        category: '1',
+        unit: '1',
+        origin_price: '1',
+        price: '1',
+        content: '1',
+        description: '1',
+        imageUrl: ['1']
       }
+    }
+  },
+  methods : {
+    addProduct() {
+      this.$emit('create-product', this.newProduct)
     }
   },
   template : '#add-product'
--- Week4_VueAndAPI/js/all.js
+++ Week4_VueAndAPI/js/all.js
@@ -5,19 +5,41 @@ var app = new Vue({
   el: '#app',
   data: {
     UUID: '354b1b67-8c78-4eab-a8f1-148bbb2f3ec1',
+    api: 'https://course-ec-api.hexschool.io/api/',
+    token: '',
     products: null
   },
   created() {
-    const vm = this;
+    this.token = document
+      .cookie
+      .replace(/(?:(?:^|.*;\s*)AUTH_TOKEN\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    axios.defaults.headers.common['Authorization'] = `Bearer ${this.token}`;
+
+    const productsPath = `${this.api}${this.UUID}/ec/products`
     axios
-      .get(`https://course-ec-api.hexschool.io/api/${this.UUID}/ec/products`)
-      .then(function (res) {
-        vm.products = res.data.data
+      .get(productsPath)
+      .then((res) => {
+        this.products = res.data.data
       })
   },
   components: {
     'products': products,
     'pagination': pagination,
     'addProduct': addProduct
+  },
+  methods: {
+    createProduct(newProduct) {
+      console.log('newProduct', newProduct);
+      const createProductPath = `${this.api}${this.UUID}/admin/ec/product`
+      axios
+        .post(createProductPath, newProduct)
+        .then(res => {
+          alert('新增完成')
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    }
   }
 });
