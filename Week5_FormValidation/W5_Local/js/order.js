9aef445b1408ce2738f6d179f564405da26e5a41
Raindot <sakuhime24@gmail.com>
2020-08-07 00:56:16 +0800

    add order api

--- Week5_FormValidation/W5_Local/js/order.js
+++ Week5_FormValidation/W5_Local/js/order.js
@@ -15,14 +15,16 @@ Vue.component('ValidationObserver', VeeValidate.ValidationObserver);
 new Vue({
   el: '#app',
   data: {
+    api: 'https://course-ec-api.hexschool.io/api/',
+    UUID: '354b1b67-8c78-4eab-a8f1-148bbb2f3ec1',
     shoppingCart: [],
+    loading: false,
     username: '',
     email: '',
     phone: '',
     address: '',
-    message: ''
-  },
-  components: {
+    payment: '',
+    message: '',
   },
   created() {
     this.getShoppingList()
@@ -32,5 +34,46 @@ new Vue({
       let localShoppingCart = localStorage.getItem('shoppingCart')
       if (localShoppingCart) this.shoppingCart = JSON.parse(localShoppingCart)
     },
+    placeOrder() {
+      this.loading = true
+      this.shoppingCart.forEach((item, idx)=> {
+        let done = false
+        if (idx + 1 === this.shoppingCart.length) done = true
+        this.addCart(item, done)
+      })
+    },
+    addCart(item, done) {
+      let createCartPath = `${this.api}${this.UUID}/ec/shopping`
+      axios.post(createCartPath, {
+        product: item.id,
+        quantity: item.count
+      }).then(res => {
+        if (done) this.createOrder()
+        console.log('addedCart', res);
+      })
+    },
+    createOrder() {
+      let createOrderPath = `${this.api}${this.UUID}/ec/orders`
+      axios.post(createOrderPath, {
+        name: this.username,
+        email: this.email,
+        tel: this.phone,
+        address: this.address,
+        payment: this.payment,
+        message: this.message
+      }).then(res => {
+        console.log(res, 'order created')
+        this.clearLocalCart()
+        this.loading = false
+        window.location.href = 'complete-order.html'
+      }).catch(err => console.dir(err) )
+    },
+    clearLocalCart() {
+      this.shoppingCart = []
+      localStorage.removeItem('shoppingCart')
+    },
+    clearCart() {
+      axios.delete(`${this.api}${this.UUID}/ec/shopping/all/product`).then(res => console.log(res))
+    }
   },
 })
